574615ada6d943c0a5edb8461f95b26cd7d27461
donihendrop <doni.hendro@ymail.com>
2016-03-18 19:23:44 +0700

    Update BerkasLamaran.java

--- JavaApplication1/src/javaapplication1/BerkasLamaran.java
+++ JavaApplication1/src/javaapplication1/BerkasLamaran.java
@@ -5,10 +5,38 @@
  */
 package javaapplication1;
 
-/**
- *
- * @author acer
- */
 public class BerkasLamaran {
+
+    private String nama;
+    private String pekerjaan;
+    private String umur;
+
+    public BerkasLamaran() {
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getPekerjaan() {
+        return pekerjaan;
+    }
+
+    public void setPekerjaan(String pekerjaan) {
+        this.pekerjaan = pekerjaan;
+    }
+
+    public String getUmur() {
+        return umur;
+    }
+
+    public void setUmur(String umur) {
+        this.umur = umur;
+    }
+    
     
 }
